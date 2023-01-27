3c76f5e4f0a3fe5118b53a59a6dcfc587e8a3a2f
mnsmns48 <115641157+mnsmns48@users.noreply.github.com>
2023-01-27 18:44:40 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,36 @@
+from dataclasses import dataclass
+from environs import Env
+
+
+@dataclass
+class TgBot:
+    admin: int
+    tg_chat: int
+    bot_token: str
+    vk_api_token: str
+    vk_token: str
+    notification: bool
+    vk_domain: str
+    amount_post_list: int
+
+
+@dataclass
+class Config:
+    tg_bot: TgBot
+
+
+def load_config(path: str = None):
+    env = Env()
+    env.read_env()
+
+    return Config(
+        tg_bot=TgBot(
+            admin=env.int("ADMIN"),
+            tg_chat=env.int("CHAT_ID"),
+            bot_token=env.str("BOT_TOKEN"),
+            vk_api_token=env.str("VK_API_TOKEN"),
+            vk_token=env.str("VK_TOKEN"),
+            notification=env.bool("DISABLE_NOTIFICATION"),
+            vk_domain=env.str("DOMAIN"),
+            amount_post_list=env.int("AMOUNT_POST_LIST")
+        ))
